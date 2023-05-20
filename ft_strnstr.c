5748c37a579183b447da34225dc4d8d34139d3cf
Shintaro Saeki <shisaeki@c1r19s11.42tokyo.jp>
2023-05-20 15:36:46 +0900

    ADD: create ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,21 @@
 /*   By: shisaeki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 15:18:18 by shisaeki          #+#    #+#             */
-/*   Updated: 2023/05/20 14:17:03 by shisaeki         ###   ########.fr       */
+/*   Updated: 2023/05/20 15:32:15 by shisaeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -27,11 +37,29 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 		j = 0;
 		while (haystack[i + j] == needle[j])
 		{
-			if (j == len_needle)
-				return ((char *)&haystack[i]);
+			if (j == len_needle - 1)
+				return ((char *)(haystack + i));
 			j++;
 		}
 		i++;
 	}
 	return (NULL);
+}
+
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+	char *s1 = "bacdefghi";
+	char *s2 = "cde";
+	char *expected;
+	char *actual;
+
+	expected = strnstr(s1, s2, 5);
+	actual = ft_strnstr(s1, s2, 5);
+
+	printf("%s\n", expected);
+	printf("%s\n", actual);
+	return (0);
 }
