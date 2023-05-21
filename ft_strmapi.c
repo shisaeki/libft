56dcad1b4693927c1eb2ec62bb4d46eb2f52e7e3
Shintaro Saeki <shisaeki@c1r23s4.42tokyo.jp>
2023-05-21 15:20:55 +0900

    ADD: create ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shisaeki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/21 15:10:17 by shisaeki          #+#    #+#             */
+/*   Updated: 2023/05/21 15:20:08 by shisaeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char *str;
+	unsigned int i;
+
+	str = (char *)malloc(sizeof(char) * ft_strlen(s));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
