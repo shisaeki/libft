e79e8af5b94be98a8704246c703dd3c788f8f8d9
ShintaroSaeki <saeki@mur4l.com>
2023-05-22 11:01:15 +0900

    ADD: ft_lstnew ft_lstadd_front ft_lstsize ft_lstlast

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shisaeki <shisaeki@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 10:43:52 by shisaeki          #+#    #+#             */
+/*   Updated: 2023/05/22 10:46:38 by shisaeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_lst;
+
+	new_lst = (t_list*)malloc(sizeof(t_list));
+	new_lst->content = content;
+	new_lst->next = NULL;
+
+	return (new_lst);
+}
